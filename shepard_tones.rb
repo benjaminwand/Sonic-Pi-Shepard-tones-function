2ddb46d931fcec776d66a4b9e308f286a2233497
Benjamin Wand <benjamin.wand@web.de>
2019-01-29 20:02:15 +0100

    better comments

--- shepard_tones.rb
+++ shepard_tones.rb
@@ -7,24 +7,14 @@ define :shepard do |pitch, time = 1, attack = 0.5, volume = 1, synth = :sine, ce
       tones = pitch % 12 + 12 * (octave + 1)    # midi number of tones
     end
     equal_amp = 30.0 / tones ** 1.5 * volume    # all at same-ish volume
-    middle =  (tones - center)/10.7             # centers around zero [-5 .. 5]
+    middle =  (tones - center)/10.7             # centers bell curve, 78 -> [-5 .. 5]
     gauss = equal_amp * 2**-( middle**2/10)     # bell curve * equalized volume
     play tones, amp: gauss, attack: attack, sustain: time, release: attack
   end
   sleep time
 end
 
-# example 1: single call with alternative synth and center
-shepard :g, 2, 0.9, 0.8, :blade, 86
-
-sleep 0.5
-
-# example 2: single call with alternative same synth and different center
-shepard :g, 2, 0.9, 0.8, :blade, 50
-
-sleep 0.5
-
-# example 3: scale call
+# example 1: scale call
 2.times do
   for each_tone in [:c, :cs, :d, :ds, :e, :f, :fs, :g, :gs, :a, :as, :b]
     shepard each_tone, 0.5
@@ -33,15 +23,15 @@ end
 
 sleep 1
 
-# example 4: call with numbers that move on a sinus curve
+# example 2: call with numbers that move on a sine wave
 x = (-200..0).map {|x| Math.sin(0.1*x)}
 for each_tone in x
   shepard each_tone, 0.05, 0.1, 1, :sine, 86
 end
 
-# example 5: rising tones and falling center
+# example 3: rising tones and falling center
 y = Range.new(0,36).step(0.1)
 z = Range.new(50,86).step(0.1).to_a.reverse
 for (each_y, each_z) in y.zip(z)
   shepard each_y, 0.1, 0.1, 1, :sine, each_z
-end 
+end
