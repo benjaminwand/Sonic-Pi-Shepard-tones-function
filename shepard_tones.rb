00c6d82dfd51666733456dffad9736c1daad0755
Benjamin Wand <benjamin.wand@web.de>
2019-01-29 20:23:42 +0100

    undoing deletions

--- shepard_tones.rb
+++ shepard_tones.rb
@@ -14,7 +14,17 @@ define :shepard do |pitch, time = 1, attack = 0.5, volume = 1, synth = :sine, ce
   sleep time
 end
 
-# example 1: scale call
+# example 1: single call with alternative synth and center
+shepard :g, 2, 0.9, 0.8, :blade, 86
+
+sleep 0.5
+
+# example 2: single call with alternative same synth and different center
+shepard :g, 2, 0.9, 0.8, :blade, 50
+
+sleep 0.5
+
+# example 3: scale call
 2.times do
   for each_tone in [:c, :cs, :d, :ds, :e, :f, :fs, :g, :gs, :a, :as, :b]
     shepard each_tone, 0.5
@@ -23,13 +33,13 @@ end
 
 sleep 1
 
-# example 2: call with numbers that move on a sine wave
+# example 4: call with numbers that move on a sine wave
 x = (-200..0).map {|x| Math.sin(0.1*x)}
 for each_tone in x
   shepard each_tone, 0.05, 0.1, 1, :sine, 86
 end
 
-# example 3: rising tones and falling center
+# example 5: rising tones and falling center
 y = Range.new(0,36).step(0.1)
 z = Range.new(50,86).step(0.1).to_a.reverse
 for (each_y, each_z) in y.zip(z)
